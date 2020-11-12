ccf4ef68433355192d03e2bba041b315f31ab512
1seok2 <misohaha31@naver.com>
2020-11-12 20:21:25 +0900

    fix error

--- src/components/utils/backgroundvideo.js
+++ src/components/utils/backgroundvideo.js
@@ -1,5 +1,5 @@
 import HomeVideo from 'assets/video/universe-1.mp4';
-import DanceVideo from '../../../dance-video.mov';
+import DanceVideo from 'assets/video/dance-video.mov';
 
 export const Video = {
   HomeVideo: HomeVideo,
